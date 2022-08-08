83b3fe122a99e5c6117407e566a9ede74ee55ff1
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-08 18:43:18 -0500

    End project issues

--- index.js
+++ index.js
@@ -1,106 +1,69 @@
-import {add} from './modules/add.js'
-import {addClassDisplay} from './modules/addClassDisplay.js'
+import {add, addClassDisplay, delet} from './modules/add.js'
+import { DateTime } from './modules/luxon.js';
+
 
 
 // Classe NewBook and the constructor
-let collectionBooks;
 class NewBook {
-
-
-  // DELET book
-  static delet(input) {
-    // Delet the number by index position
-    collectionBooks.splice(input, 1);
-
-    // Upgrade the DOM
-    NewBook.printf(collectionBooks);
-
-    // Upgrade the Local Storage
-    localStorage.setItem('data', JSON.stringify(collectionBooks));
-
-    return collectionBooks;
-  }
-
   // Print the info in to the browser
   static printf(input) {
     document.getElementById('container-book').innerHTML = input.map((items, index) => `
     <div id="cards">
     <p>"${items.title}" by ${items.author} </p>
-    <button class="buttonRemove" onclick="NewBook.delet(${index})">Remove</button>
+    <button id='buttonremove${index}' class="buttonRemove" type="submit">Remove</button>
     </div>
-    `).join('');
+    `);
   }
 
-  static getBooks() {
-    let books = [];
-    if (localStorage.getItem('data')) {
-      books = JSON.parse(localStorage.getItem('data'));
-    }
-    return books;
-  }
+  // static getBooks() {
+  //   let books = [];
+  //   if (localStorage.getItem('data')) {
+  //     books = JSON.parse(localStorage.getItem('data'));
+  //   }
+  //   return books;
+  // }
 
   // Get date
   static sendDate() {
-    const hora = Array.from(Date());
-    let ordinal;
-
-    const dayWeek = hora.splice(0, 3).join(''); // día de la semana
-    const monthAndDay = hora.splice(1, 6).join(''); // Mes y día
-    const year = hora.splice(2, 4).join(''); // Año
-    const hour = hora.splice(3, 8).join(''); // Hora, minutos y segundos
-
-    if (monthAndDay.substring(4, 6) === 1 || monthAndDay.substring(4, 6) === 21) {
-      ordinal = 'st';
-    } if (monthAndDay.substring(4, 6) === 2 || monthAndDay.substring(4, 6) === 22) {
-      ordinal = 'nd';
-    } if (monthAndDay.substring(4, 6) <= 3 || monthAndDay.substring(4, 6) === 23) {
-      ordinal = 'rd';
-    }
-
-    if (monthAndDay.substring(4, 6) <= 20 && monthAndDay.substring(4, 6) > 3) {
-      ordinal = 'th';
-    } if (monthAndDay.substring(4, 6) >= 24 && monthAndDay.substring(4, 6) <= 31) {
-      ordinal = 'th';
-    }
-    document.getElementById('date').innerHTML = `${dayWeek}, ${monthAndDay}${ordinal} ${year}, ${hour}`;
+    const myDate = DateTime.now();
+    document.getElementById('date').innerHTML =  myDate.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
   }
-
-
 }
-
-
-
 
 // Call get date
 window.addEventListener('DOMContenLoaded', NewBook.sendDate());
 setInterval(NewBook.sendDate, 1000);
 
-// Get information from LocalStorage in to browser
-collectionBooks = NewBook.getBooks();
-NewBook.printf(collectionBooks);
+window.addEventListener('DOMContenLoaded', NewBook.printf(add()));
+
 
 // Save info in LocalStorage
-document.querySelector('#bookForm').addEventListener('submit', () => {
-  localStorage.setItem('data', JSON.stringify(collectionBooks));
+// document.querySelector('#bookForm').addEventListener('submit', () => {
+//   localStorage.setItem('data', JSON.stringify(collectionBooks));
 
-  // Clean inputs info
-  document.querySelector('#input-title').value = '';
-  document.querySelector('#input-author').value = '';
-});
+//   document.querySelector('#input-title').value = '';
+//   document.querySelector('#input-author').value = '';
+// });
+
 
 
 
 // Call the function to add classes to the main sections
 addClassDisplay('addNew');
+document.getElementById('add-book').addEventListener('click', () => NewBook.printf(add()));
 
-
-
-document.getElementById('add-book').addEventListener('click', () => {
-  NewBook.printf(add())
-});
-
-
-// Links para cambiar pantallas
+// Links to changes the windows
 document.getElementById('nav-list').addEventListener('click', () => addClassDisplay('list'));
 document.getElementById('nav-new').addEventListener('click', () => addClassDisplay('addNew'));
 document.getElementById('nav-contact').addEventListener('click', () => addClassDisplay('contact'));
+
+// Match the remove button
+const booksContainer = document.getElementById('container-book');
+booksContainer.addEventListener('click', (event) => {
+  const id = event.target.id;
+  const regex = /(?<=buttonremove)\d+$/;
+  if(regex.test(id)) {
+    const index = id.match(regex)[0];
+    NewBook.printf(delet(index));
+  }
+});
--- modules/add.js
+++ modules/add.js
@@ -1,7 +1,7 @@
 
   // ADD book
-  const collectionBooks = [];
-
+  let collectionBooks = [];
+ 
   class NewConstructor {
     constructor(title, author) {
       this.title = title;
@@ -9,23 +9,86 @@
     }
   }
 
-
-
   const add = () => {
-                                                                                                 console.log('Función llamada')
-    const newtitle = document.querySelector('#input-title').value;
-    const newauthor = document.querySelector('#input-author').value;
-                                                                                              console.log('titulo:', newtitle);
-                                                                                              console.log('newauthor:', newtitle);
-    // If the title or the author is empty, then don't add the book
-    if (newtitle === '' || newauthor === '') {
-      return null;
+  
+    if (localStorage.getItem('data')) {
+      let books = JSON.parse(localStorage.getItem('data'));
+      collectionBooks = books;
     }
     
+
+    const newtitle = document.querySelector('#input-title').value;
+    const newauthor = document.querySelector('#input-author').value;
+
+    // If the title or the author is empty, then don't add the book
+    if (newtitle === '' || newauthor === '') {
+      return collectionBooks;
+    }
+
+    console.log('Recarga página', collectionBooks);
+
     // Create a new Book
     const bookInfo = new NewConstructor(newtitle, newauthor);
     collectionBooks.push(bookInfo);
+
+    localStorage.setItem('data', JSON.stringify(collectionBooks));
+
+    document.querySelector('#input-title').value = '';
+    document.querySelector('#input-author').value = '';
+
+    console.log(collectionBooks);
+
     return collectionBooks;
   }
 
-export {add}
+    const delet = (input) => {
+  console.log('Boton de borrar llamado',input);
+
+  // Delet the number by index position
+  collectionBooks.splice(input, 1);
+
+  // Upgrade the Local Storage
+  localStorage.setItem('data', JSON.stringify(collectionBooks));
+  return collectionBooks;
+  }
+
+    const addClassDisplay = (input) => {
+      switch (input) {
+      case 'list':
+      document.getElementById('listContainer').style.display = '';
+      document.getElementById('formContainer').style.display = 'none';
+      document.getElementById('contactContainer').style.display = 'none';
+
+      document.getElementById('nav-list').classList.add('active');
+      document.getElementById('nav-new').classList.remove('active');
+      document.getElementById('nav-contact').classList.remove('active');
+      break;
+
+      case 'addNew':
+      document.getElementById('listContainer').style.display = 'none';
+      document.getElementById('formContainer').style.display = '';
+      document.getElementById('contactContainer').style.display = 'none';
+
+      document.getElementById('nav-list').classList.remove('active');
+      document.getElementById('nav-new').classList.add('active');
+      document.getElementById('nav-contact').classList.remove('active');
+      break;
+
+      case 'contact':
+      document.getElementById('listContainer').style.display = 'none';
+      document.getElementById('formContainer').style.display = 'none';
+      document.getElementById('contactContainer').style.display = '';
+
+      document.getElementById('nav-list').classList.remove('active');
+      document.getElementById('nav-new').classList.remove('active');
+      document.getElementById('nav-contact').classList.add('active');
+      break;
+
+      default:
+      break;
+    }
+  }
+
+
+
+export {add, delet, addClassDisplay};
